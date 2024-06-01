013350accde57e9a033c3833313538068c52b823
Ruslan <rustamovruslan1205@gmail.com>
2024-06-01 09:18:07 +0400

    initial

---
+++ apps/server/src/prisma/prisma.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
+  }
+
+  onModuleInit(): any {
+    this.$connect();
+  }
+
+  onModuleDestroy(): any {
+    this.$disconnect();
+  }
+}
