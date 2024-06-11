86938eda40ecbdda554b6003f6b3e82c57018967
Ruslan <rustamovruslan1205@gmail.com>
2024-06-11 12:51:55 +0400

    fixed

--- apps/server/src/auth/auth.module.ts
+++ apps/server/src/auth/auth.module.ts
@@ -3,11 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { AtStrategy, RtStrategy } from '../strategies';
-import { MailService } from '../mail/mail.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, AtStrategy, RtStrategy, MailService],
+  providers: [AuthService, AtStrategy, RtStrategy],
   imports: [JwtModule.register({})],
 })
 export class AuthModule {}
