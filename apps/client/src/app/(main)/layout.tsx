6b74535fe02c215aedb580e190355075b827df02
Ruslan <rustamovruslan1205@gmail.com>
2024-06-17 15:03:07 +0400

    test

--- apps/client/src/app/(main)/layout.tsx
+++ apps/client/src/app/(main)/layout.tsx
@@ -1,11 +1,22 @@
+"use client";
 import Sidebar from "@/app/(main)/components/layout/sidebar";
 import Providers from "@/app/providers";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { push } = useRouter();
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (!localStorage.getItem("token")) {
+        push("/sign-in");
+      }
+    }
+  }, []);
   return (
     <div className={"flex max-h-[100vh] max-w-[100vw] overflow-hidden"}>
       <Providers>
