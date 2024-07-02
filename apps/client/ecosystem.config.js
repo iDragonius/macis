038bfd86c8d8b2a086496adc4cd301a916c10704
Ruslan <rustamovruslan1205@gmail.com>
2024-07-02 09:49:57 +0400

    deployment

---
+++ apps/client/ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    {
+      name: "macis",
+      script: "yarn start",
+      port: 3001,
+    },
+  ],
+};
---
+++ apps/server/ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    {
+      name: 'macis-api',
+      script: 'yarn start',
+      port: 4000,
+    },
+  ],
+};
