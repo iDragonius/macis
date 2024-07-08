b590e8e11fa6e66964595e04fd1432b1f25b41e5
Ruslan <rustamovruslan1205@gmail.com>
2024-07-08 14:08:01 +0400

    touches

--- apps/client/src/lib/error-messages.ts
+++ apps/client/src/lib/error-messages.ts
@@ -2,4 +2,5 @@ export const ErrorMessages: Record<string, string> = {
   wrongPassword: "Şifrə səhvdir!",
   userNotFound: "İstifadəçi tapılmadı!",
   managerMonthlyTargetAlreadyExist: "Menecer üçün aylıq hədəf artıq mövcuddur!",
+  customerAlreadyExist: "Bu şirkət adı ilə müştəri artıq mövcuddur!",
 };
